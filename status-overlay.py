27f4698addc960fac008002eecabf917b231b4c1
Piotr Majkrzak <piotr@majkrzak.dev>
2021-09-25 16:50:29 +0200

    Add basic status display

--- status-overlay.py
+++ status-overlay.py
@@ -1,8 +1,30 @@
-from obspython import obs_properties_create
+from obspython import (
+    obs_properties_create,
+    obs_properties_add_bool,
+    timer_add,
+    timer_remove,
+    obs_frontend_recording_active,
+    obs_frontend_streaming_active,
+)
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtCore import Qt
 
 window: QLabel
+
+
+def get_status() -> str:
+    return {
+        (False, False): "IDLE",
+        (False, True): "AIR",
+        (True, False): "REC",
+        (True, True): "REC&AIR",
+    }[(obs_frontend_recording_active(), obs_frontend_streaming_active())]
+
+
+def update() -> None:
+    global window
+    window.setText(f"{get_status()}")
+    window.adjustSize()
 
 
 def script_description() -> str:
@@ -31,6 +53,8 @@ def script_load(settings: "obs_data_t *") -> None:
         | Qt.WindowTransparentForInput
     )
 
+    timer_add(update, 100)
+
 
 def script_update(settings: "obs_data_t *") -> None:
     global window
@@ -40,4 +64,5 @@ def script_update(settings: "obs_data_t *") -> None:
 
 def script_unload() -> None:
     global window
+    timer_remove(update)
     window.destroy()
